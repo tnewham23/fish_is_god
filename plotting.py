fcc6f122544eca4ad79f113c698ff630443ba23a
Thomas Newham <thomasnewham@192-168-1-114.tpgi.com.au>
2021-07-06 23:31:07 +1000

    rsi plotting for instrument

--- plotting.py
+++ plotting.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import rsi
+import sys
 
 # copied from eval.py
 def loadPrices(fn):
@@ -15,6 +17,8 @@ def loadPrices(fn):
 def plot_instance(prcHist, stock_indx, list_of_metrics=None, metric_names=None):
     
     df = pd.DataFrame(prcHist[stock_indx])
+
+    plot_a = plt.figure(1)
     
     plt.plot(prcHist[stock_indx], label="price")
     
@@ -35,14 +39,43 @@ def plot_instance(prcHist, stock_indx, list_of_metrics=None, metric_names=None):
     plt.xlabel("date")
     plt.ylabel("$ price")
     plt.title(f"Stock: {stock_indx}")
-    plt.show()
+    #plt.show()
+
+# takes prcHist as numpy array
+# stock_indx as the instrument of interest
+def plot_rsi_instance(prcHist, stock_indx):
+    x = []
+    plot_b = plt.figure(2)
+    # generate rsi values for each day
+    for i in range(0, 250):
+        if i == 0:
+            x.append(50)
+            continue
+        prcHistSoFar = prcHist[:,:i]
+        x.append(rsi.ins_rsi(prcHistSoFar[stock_indx], 14))
+    
+    plt.plot(x, label='RSI')
+    plt.axhline(y=30, color = 'r', linestyle = '-')
+    plt.axhline(y=70, color = 'r', linestyle = '-')
+
+    plt.legend()
+    plt.xlabel("Date")
+    plt.ylabel("RSI")
+    plt.title(f"RSI of Stock: {stock_indx}")
+
+    
+
+
 
 # example with SMA function
 def SMA(data, window):
     return pd.DataFrame(data)[0].rolling(window=window).mean()
 
+
 if __name__ == '__main__':
     nInst, nt = 0, 0
     prcHist = loadPrices("prices250.txt")
 
-    plot_instance(prcHist, 8, [lambda x : SMA(x, 10)], ["SMA10"])
+    plot_instance(prcHist, 15, [lambda x : SMA(x, 10)], ["SMA10"])
+    plot_rsi_instance(prcHist, 15)
+    plt.show()
